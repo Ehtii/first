3281c1c99c4cba0c02e4471ccfc3cf1b24d4755b
Ehtii <63145855+Ehtii@users.noreply.github.com>
2020-04-04 11:55:04 +0200

    Create first.py

---
+++ first.py
@@ -0,0 +1,24 @@
+def count(lst):
+    global more
+    global less
+    for i in range(len(lst)):
+        if len(lst[i]) > 5:
+            more=more+1
+        elif len(lst[i])<5:
+            less=less+1
+    
+    return more, less
+
+lst =[]
+x=int(input("how many elements"))
+for i in range(x):
+    ele=input("enter a name")
+    lst.append(ele)
+
+more=0
+less=0
+
+more,less=count(lst)
+print(lst)
+print(more)
+print(less)
